d747832cadaab3421470721770ef84cd62117524
Rafael Freitas <passoca@pop-os.localdomain>
2020-12-02 17:55:35 -0300

    feat: adding ts

--- src/services/apiCards.ts
+++ src/services/apiCards.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiCards = axios.create({
+export default axios.create({
   baseURL: 'http://my-json-server.typicode.com/Rafonel/niok',
   withCredentials: false,
   headers: {
@@ -8,9 +8,3 @@ const apiCards = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-export default {
-  getEvents() {
-    return apiCards.get('/cards');
-  },
-};
